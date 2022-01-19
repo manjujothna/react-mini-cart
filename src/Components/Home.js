b71f54bfe5f37340859b7409c1bd2dafdf044fa6
manjujothna <manju22pavi@gmail.com>
2022-01-18 18:28:31 -0800

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ItemCard from './ItemCard';
+import data from '../data';
+
+const Home = ()=>
+{
+    //const {data} = data;
+    console.warn(data.productData);
+    return(
+        <>
+        <h1 className="text-center mt-3">All Items</h1>
+        <section className="py-4 container">
+            <div className="row justify-content-center">
+                {
+                    data.productData.map((item, index)=>
+                    {
+                        return(
+                            <ItemCard 
+                            img= {item.img}
+                            title= {item.title}
+                            desc= {item.desc} 
+                            price= {item.price}
+                            item={item} 
+                            key={index}
+                            />
+                        )
+                    })
+                }
+                
+            </div>
+
+        </section>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+
+const data=
+{
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: "Juicy Grapes",
+            desc:"",
+            price: 46
+        },
+        {
+            id: 2,
+            img: img2,
+            title: "Red Watemelon",
+            desc:"",
+            price: 56
+        },
+        {
+            id: 3,
+            img: img3,
+            title: "Juicy Orange",
+            desc:"",
+            price: 48
+        }
+    ]
+}
+
+export default data;
